04f6ad808dab4edd51a484d7d71b4ed300ec7a09
bujue <tougusheng@gmail.com>
2014-07-24 10:34:19 +0800

    Finish user edit,update,index,and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,71 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user,only: [:edit,:update,:index]
+  before_action :correct_user,only: [:edit,:update]
+  before_action :admin_user,only: :destory
   def new
     @user = User.new
   end
 
-  def create
-   @user = User.new(user_params)
-   if @user.save
-     sign_in @user
-     flash[:success] ="Welcome to the Sample App"
-     redirect_to @user
-   else
-     render 'new'
-    end
+  def index
+    @users= User.paginate(page: params[:page])
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] ="Welcome to the Sample App"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
   def user_params
-   params.require(:user).permit(:name, :email, :password,
-                               :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 
   def show
     @user=User.find(params[:id])
   end
+
+  def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success]="profile updated"
+      sign_in @user
+      redirect_to @user
+
+    else
+      render'edit'
+    end
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url,notice:"please sign in."unless signed_in?
+    end
+  end
+
+  def correct_user
+    @user= User.find(params[:id])
+    redirect_to(root_path)unless current_user?(@user)
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success]="User destroyed."
+    redirect_to users_url
+  end
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
+
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -33,7 +35,7 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
-
+  config.include RSpec::Rails::RequestExampleGroup, type: :feature
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
